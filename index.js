1f07be7ea01727a526a01a594ea6de441644b785
DylanHedges <d.hedges1@hotmail.co.uk>
2017-12-20 09:49:02 +0000

    Added Passportjs and Google OAuth passport strategies

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-//On the back end we use a module system common JS modules (a system in node js that allows code to be shared between files), on the front end (React) we use "import" a different module system called ES2015 modules (node JS does not have support for this hence the different syntax)
+//On the back end we use a module system called common JS modules (a system in node js that allows code to be shared between files), on the front end (React) we use "import" a different module system called ES2015 modules (node JS does not have support for this hence the different syntax)
 const express = require('express');
+const passport = require('passport');
+//Save only the "Strategy" property from 'passport-google-oauth20' into the "GoogleStrategy" variable
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 //Creates a new express app (object) - defines config that listens to incoming requests from Node and send them to different route handlers
 const app = express();
 
-//"app" object - represents the underlying express server which has route handlers associated with it, ".get" - creates new route handler using the HTTP GET method (could also be .POST, .PUT, .DELETE, .PATCH), '/' - route portion of handler express sends request to route specified here, "req" - a JS object that represents the incoming request (data about who is making the request), "res" - a JS object that represents the data being sent back
-//"res.send({hi: "there"})" - arrow function is called by express every time a request comes into this route, the body of this function tells express to send back some JSON to whoever made the request
-app.get('/', (req, res) => {
-	res.send({ hi: 'there' });
-});
+//"passport.use()" - makes Passport aware of whatever is inside the () in this case the Google strategy,"new GoogleStrategy()" - Creates a new instance of the Google Passport strategy i.e a new user wants to authenticate with Google
+passport.use(new GoogleStrategy());
 
 //"const PORT = process.env.PORT" - Allows Heroku (AWS etc.) to dynamically define which port number Node listens to when we deploy our app, looks in the underlying runtime below node and finds the correct port to listen on, "|| 5000" - Development port, if we are on our local machine we hard code the port we want node to listen
 const PORT = process.env.PORT || 5000;
