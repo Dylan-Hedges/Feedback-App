a67b7907584e9fb899cdc9cef5ec7b29f1767910
DylanHedges <d.hedges1@hotmail.co.uk>
2018-01-23 12:36:38 -0500

    Installed React, Redux, React-Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App';
+
+//Creates Redux store - takes reducers, the inital state of app, middleware
+const store = createStore(() => [], {}, applyMiddleware());
+
+//Renders root component in DOM - Takes root component (< /> are jsx tags - these tags create an instance of the component which ReactDOM expects), and where we want to render it to inside of our DOM (we refrence id="root" inside of our public->index.html file as this is where everything is rendered to)
+ReactDOM.render(
+	//Links Redux store <-> React side of our app using the <Provider> tags (this is a react component that knows how to read changes from our Redux store, Provider will inform all of its children components of new states/state changes in the Redux store)
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.querySelector('#root')
+);
