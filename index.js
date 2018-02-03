0bd0f18170aa67d808fb313b59c15c6386077406
DylanHedges <d.hedges1@hotmail.co.uk>
2018-02-03 09:26:33 -0500

    Added Heroku deployment config

--- index.js
+++ index.js
@@ -37,6 +37,18 @@ require('./routes/authRoutes')(app);
 //Import billing route function into our app - Imports in the function from billingRoutes.js and immediately exports it to our express "(app)" object
 require('./routes/billingRoutes')(app);
 
+//If in the production/Heroku
+if (process.env.NODE_ENV === 'production') {
+	//Check to see if there is a specific file in 'client/build' folder that matches the request - Searches for the main JS/CSS file in the 'client/build' folder
+	app.use(express.static('client/build'));
+	//Requires in the 'path' module
+	const path = require('path');
+	//If its not one of our specified routes AND its not in 'client/build' send back the 'index.html' file - Sends "index.html" for any routes we dont understand - sends the request over to the client side, some of our routes are only specified in our client side
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 //"const PORT = process.env.PORT" - Allows Heroku (AWS etc.) to dynamically define which port number Node listens to when we deploy our app, looks in the underlying runtime below node and finds the correct port to listen on, "|| 5000" - Development port, if we are on our local machine we hard code the port we want node to listen
 const PORT = process.env.PORT || 5000;
 //Tells Express to tell Node to listen to traffic on the number assigned in the PORT variable
