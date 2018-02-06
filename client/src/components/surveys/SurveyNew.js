f9fab39cb641ec2910150306c1c8d989c73d650d
DylanHedges <d.hedges1@hotmail.co.uk>
2018-02-06 10:23:55 -0500

    Added Redux Form, added new survey component

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class SurveyNew extends Component {
+	render() {
+		return <div>SurveyNew!</div>;
+	}
+}
+
+export default SurveyNew;
+
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,12 @@
 //Combines individual reducers
 import { combineReducers } from 'redux';
+//Imports the reducer created by Redux Form - allows us to use all of its features in our app, the name of the function is 'reducer' which isnt very descriptive, es2015 module imports allow us to rename imported variable to be called "reduxForm" by adding in "as"
+import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
 
 //Create and export combineReducers call
 export default combineReducers({
-	//Assigns the "authReducer" to the "auth" property
-	auth: authReducer
+	//Assigns the the output of "authReducer" to the "auth" key (every reducer has to have a key, the output of the reducer will be in the Redux store under this K/V pair)
+	auth: authReducer,
+	form: reduxForm
 });
